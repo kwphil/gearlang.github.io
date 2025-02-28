eb50b0c42eb8a56181bbc62d440d98eea649b5f5
kwphil <phillipsw1980@gmail.com>
2025-02-28 10:26:22 -0600

    Basic

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <>
-
-    </>    
+    <div className="App">
+      <header>
+        <div className="text">
+          <h1>GearLang</h1>
+          <h3>A Clean Systems Programming Language</h3>
+        </div>
+        <image href="gearlang_logo.png"></image>
+      </header>
+    </div>    
   );
 }
 
